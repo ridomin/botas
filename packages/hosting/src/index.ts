f2a2461b71013e9e51b9a5b7dd217c603588eb7c
Rido <rido-min@users.noreply.github.com>
2024-10-28 23:12:22 +0000

    initial import

--- packages/hosting/src/index.ts
+++ packages/hosting/src/index.ts
@@ -1,6 +1,31 @@
-import { getRandomGuid } from '@rido-min/core'
+import { Activity } from '@rido-min/core'
 
 export function start (): void {
-  const id = getRandomGuid()
-  console.log('Starting hosting (6)...', id)
+  const myActivity : Activity = {
+    callerId : 'callerid',
+    serviceUrl : 'serviceurl',
+    type : 'activity',
+    localTimezone : 'localtimezone',
+    channelId : 'channelid',
+    from : {
+      id : 'fromid',
+      name : 'fromname',
+    },
+    conversation : {
+      id : 'conversationid',
+      name : 'conversationname',
+      conversationType : 'conversationtype',
+      isGroup : true,
+    },
+    recipient : {
+      id : 'recipientid',
+      name : 'recipientname',
+    },
+    text : 'text',
+    label : 'label',
+    valueType: 'valueType',
+    listenFor: []
+  }
+    
+  console.log('basic activity', myActivity)
 }
