cca8fa2a907fdd20d9126387e027c452f009aca4
Rido <rido-min@users.noreply.github.com>
2024-10-28 10:58:43 -0700

    test v5 (#19)

--- packages/hosting/src/index.ts
+++ packages/hosting/src/index.ts
@@ -2,5 +2,5 @@ import { getRandomGuid } from '@rido-min/core'
 
 export function start (): void {
   const id = getRandomGuid()
-  console.log('Starting hosting...', id)
+  console.log('Starting hosting (5)...', id)
 }
