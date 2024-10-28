4376a1e4c37890958496ac338948768775b644c3
Rido <rido-min@users.noreply.github.com>
2024-10-28 12:54:57 -0700

    Fix/no star (#20)
    * reset pkg versions
    
    * pretest
    
    * reset versions
    
    * update deps
    
    * add clean and star to sample

--- setVersion.js
+++ setVersion.js
@@ -1,7 +1,29 @@
 import * as nbgv from 'nerdbank-gitversioning'
 import fs from 'fs'
+import path from 'path'
 
-const setPackageVersionAndBuildNumber = (versionInfo) => {
+
+const updateDeps = (p, v) => {
+  if (!fs.existsSync('package-lock.json')) {
+    console.error('package-lock.json not found')
+    return
+  }
+  const pkgLock = JSON.parse(fs.readFileSync('package-lock.json', 'utf8'))
+  const packageJsonPath = path.join(p, 'package.json')
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath , 'utf8'))
+  const deps = packageJson.dependencies
+  for (const dep in deps) {
+    const lockNode = pkgLock.packages[`node_modules/${dep}`]
+    if (lockNode && lockNode.link) {
+      deps[dep] = v
+    } else {
+      console.log(dep + ' Not a link')
+    }
+  }
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+}
+
+const setPackageVersionAndBuildNumber = versionInfo => {
   // Set a build output value representing the NPM package version
   console.log('::set-output name=package_version::' + versionInfo.npmPackageVersion)
   console.log('##vso[build.updatebuildnumber]' + versionInfo.npmPackageVersion)
@@ -14,7 +36,10 @@ const setPackageVersionAndBuildNumber = (versionInfo) => {
     const folders = files
       .filter(file => file.isDirectory())
       .map(folder => `${folder.parentPath}/${folder.name}`)
-    folders.forEach(f => nbgv.setPackageVersion(f))
+    folders.forEach(f => {
+      updateDeps(f, versionInfo.npmPackageVersion)
+      nbgv.setPackageVersion(f)
+    })
   })
 }
 
