7b9f86d146996af88c19c550e1af3edacfb79df5
ridomin <ridomin@noemail-mbp.com>
2026-04-26 23:18:27 -0700

    one liner

--- node/samples/echo-bot/index.ts
+++ node/samples/echo-bot/index.ts
@@ -3,10 +3,16 @@
 
 import { BotApp } from 'botas-express'
 
+// new BotApp()
+//   .on('message', 
+//     ctx => ctx.send(`You said: ${ctx.activity.text}`))
+//   .start()
+
 const app = new BotApp()
 
-app.on('message', async (ctx) => {
-  await ctx.send(`You said: ${ctx.activity.text}`)
-})
+app.on('message', async ctx => {
+    await ctx.send(`You said: ${ctx.activity.text}`)
+  }
+)
 
 app.start()
